8239979a9b2c795b669d73d6a9e8e268707b59f8
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2023-07-28 03:22:47 -0400

    sumOfInteger exercise

--- src/Exercises/sumOfInteger/Main.java
+++ src/Exercises/sumOfInteger/Main.java
@@ -1,5 +1,19 @@
 package Exercises.sumOfInteger;
 
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+
+        int n = s.nextInt();
+        int sum = 0;
+        while(n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        System.out.println("sum " + sum);
+    }
 }
