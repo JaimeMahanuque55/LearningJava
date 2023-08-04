f054646bc6a2f6d891ec436d2db2fcde7ed594ce
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2023-08-04 03:52:47 -0400

    a program that displays the maximum and minimum elements of an array.

---
+++ src/Exercises/maxAndMinArray/Main.java
@@ -0,0 +1,17 @@
+package Exercises.maxAndMinArray;
+//Write a program that displays the maximum and minimum elements of an array.
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {2, 4, 6, -3, 5};
+
+        int min = numbers[0];
+        int max = numbers[0];
+
+        for (int i = 1; i < numbers.length; i++) {
+            max = (numbers[i] > max) ? numbers[i] : max;
+            min = (numbers[i] < min) ? numbers[i] : min;
+        }
+
+        System.out.println("Max= " + max + ", min= " + min);
+    }
+}
