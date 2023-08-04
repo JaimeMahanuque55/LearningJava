c5aca88b2e0587f9fc73515953203d9d0c4b92b2
Jaime Mahanuque <jaimemahanuque55@gmail.com>
2023-08-04 04:09:22 -0400

    a program that places the odd elements of an array before the even elements.

---
+++ src/Exercises/placeOddBeforeEvenELements/Main.java
@@ -0,0 +1,30 @@
+package Exercises.placeOddBeforeEvenELements;
+
+import java.util.Arrays;
+
+// write a program that places the odd elements of an array before the even elements.
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 4, 5, 7, 9, -2, 3};
+
+        int[] temp = new int[numbers.length];
+        int j = 0;
+        int k = numbers.length - 1;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 != 0) {
+                temp[j++] = numbers[i];
+            } else {
+                temp[k--] = numbers[i];
+            }
+        }
+        copyArray(temp, numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    public static void copyArray(int[] temp, int[] numbers) {
+        for (int i = 0; i < temp.length; i++) {
+            numbers[i] = temp[i];
+        }
+    }
+}
